970619a1db9aa02a96346e495b56ef410649bf5c
Mika <jmj990411@1163.com>
2020-08-14 23:21:43 +0800

    Revert "Update RecordView.swift"
    This reverts commit eeab06ef8af8125a24aae1df34d5c113e78f1394.

--- SparklingEye/RecordView.swift
+++ SparklingEye/RecordView.swift
@@ -80,13 +80,11 @@ struct EyesightRecordCardView: View {
                     Spacer()
                     RoundedRectangle(cornerRadius: 12)
                         .frame(width: 164, height: 60)
-                        .aspectRatio(164 / 60.0 , contentMode: .fit)
                         .foregroundColor(Color(#colorLiteral(red: 0.7450980392, green: 0.8901960784, blue: 0.8588235294, alpha: 1)))
                         .offset(y: 20)
                 }
                 RoundedRectangle(cornerRadius: 12)
-//                    .frame(width: 342, height: 133)
-                    .aspectRatio(342 / 133.0 , contentMode: .fit)
+                    .frame(width: 342, height: 133)
                     .foregroundColor(Color(#colorLiteral(red: 0.7450980392, green: 0.8901960784, blue: 0.8588235294, alpha: 1)))
                 
             }.padding()
